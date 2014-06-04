03bd7e87bc1cadfc04d671f9f8ab12a6bb109e0e
Joseph Finlayson <joseph.finlayson@gmail.com>
2014-06-04 21:35:30 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-#configuring the system
-wget https://raw.github.com/arunoda/travis-ci-laika/master/Makefile
-
 #install meteor
 curl https://install.meteor.com | /bin/sh
 
 #installing meteorite
 npm install -g meteorite
 
-#installing laika
+#installing deps
 mrt update
